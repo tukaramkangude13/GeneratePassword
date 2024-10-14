2047e461e7848be5230d3636493fb59fd0a5aabd
tukaramkangude13 <tukaramkangude13@gmail.com>
2024-10-14 12:27:07 +0530

    this is

---
+++ index.js
@@ -0,0 +1,16 @@
+const  inputslider= document.querySelector("lenght-slider");
+const leangthDisplay=document.querySelector("lenght-number");
+
+const passwordDisplay=document.querySelector("display");
+const copybtn=document.querySelector("[data-copy]");
+const copymsg=document.querySelector("[data-copyMsg]");
+
+const uppercase=document.getElementById("Uppercase");
+const lowercase=document.getElementById("#Lowercase");
+const symbol=document.getElementById("#Symbol");
+const number=document.getElementById("#Number");
+const allCheckbox=document.querySelectorAll(" input[type=checkbox]");
+
+
+const dataindicator=document.getElementById("#indicator");
+  const generate=document.getElementById(".generatebutton");
